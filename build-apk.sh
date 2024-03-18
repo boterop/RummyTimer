9634d71b5fc86e169621f83d3e1a37f6a4443b38
Santiago Botero <boterop22@gmail.com>
2024-03-18 16:09:21 -0500

    Add icon

--- build-apk.sh
+++ build-apk.sh
@@ -1,3 +1,4 @@
+mkdir android/app/src/main/assets
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 cd android
 ./gradlew assembleDebug
