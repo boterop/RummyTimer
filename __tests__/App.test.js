9c4a5f6c779ddb05360f1e18fc086c9e02354052
Santiago Botero <boterop22@gmail.com>
2024-03-19 01:21:27 -0500

    Create tests

---
+++ __tests__/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Home } from '../src/screens';
+import { create } from 'react-test-renderer';
+import App from '../src/App';
+
+it('Home should be the first screen', () => {
+	const result = create(<App />).toJSON();
+	const home = create(<Home />).toJSON();
+
+	expect(JSON.stringify(result)).toMatch(JSON.stringify(home));
+});
---
+++ __tests__/components/Clock.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Clock } from '../../src/components';
+import { create } from 'react-test-renderer';
+
+describe('Show correctly the seconds in minutes:seconds', () => {
+    it('01:00', () => {
+        const time = 60;
+        const expectedHour = "01:00";
+        const component = create(<Clock time={time} />).toJSON();
+
+        expect(JSON.stringify(component)).toContain(expectedHour);
+    });
+
+    it('01:59', () => {
+        const time = 119;
+        const expectedHour = "01:59";
+        const component = create(<Clock time={time} />).toJSON();
+
+        expect(JSON.stringify(component)).toContain(expectedHour);
+    });
+
+    it('02:05', () => {
+        const time = 125;
+        const expectedHour = "02:05";
+        const component = create(<Clock time={time} />).toJSON();
+
+        expect(JSON.stringify(component)).toContain(expectedHour);
+    });
+    
+    it('10:00', () => {
+        const time = 600;
+        const expectedHour = "10:00";
+        const component = create(<Clock time={time} />).toJSON();
+
+        expect(JSON.stringify(component)).toContain(expectedHour);
+    });
+})
