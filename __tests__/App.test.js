a936d2713437ef8d5dcdf54d0bd3b620db8b8e82
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-03-19 23:06:16 -0500

    Tests working with mock media (#20)

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Home } from '../src/screens';
 import { create } from 'react-test-renderer';
 import App from '../src/App';
+import MockMedia from './__mocks__/Media';
 
 it('Home should be the first screen', () => {
-	const result = create(<App />).toJSON();
-	const home = create(<Home />).toJSON();
+	const result = create(<App mockMedia={MockMedia} />).toJSON();
+	const home = create(<Home mockMedia={MockMedia} />).toJSON();
 
 	expect(JSON.stringify(result)).toMatch(JSON.stringify(home));
 });
--- src/services/Media.js
+++ src/services/Media.js
@@ -2,10 +2,7 @@ import Sound from 'react-native-sound';
 
 let sound = null;
 
-const getPermission = async () => true;
-
 const Media = {
-	getPermission,
 	play: async uri => {
 		if (sound == null) {
 			sound = new Sound(uri, Sound.MAIN_BUNDLE);
